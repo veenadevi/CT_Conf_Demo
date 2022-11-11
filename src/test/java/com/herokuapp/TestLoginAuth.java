8a9775619dfdb032921b17eebb12861e351634cd
veenadevi <veenadevi.s@gmail.com>
2022-11-11 06:40:32 +0530

    Initial Commmit

---
+++ src/test/java/com/herokuapp/TestLoginAuth.java
@@ -0,0 +1,66 @@
+package com.herokuapp;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestLoginAuth {
+
+    WebDriver driver;
+    @Test
+    public void testInvalidAuth(){
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        // options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(120,TimeUnit.SECONDS);
+
+        driver.get("http://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.cssSelector("#username")).sendKeys("Veena");
+        driver.findElement(By.cssSelector("#password")).sendKeys("mypassword");
+        driver.findElement(By.cssSelector(".fa-sign-in")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector(".error")).isDisplayed());
+    }
+
+    @Test
+    public void testValidAuth(){
+
+//        WebDriverManager.chromedriver().setup();
+//        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--disable-dev-shm-usage");
+//        // options.addArguments("--headless");
+//        driver = new ChromeDriver(options);
+//        driver.manage().window().maximize();
+//
+//        driver.manage().timeouts().implicitlyWait(120,TimeUnit.SECONDS);
+
+        driver.get("http://the-internet.herokuapp.com/login");
+
+        driver.findElement(By.cssSelector("#username")).sendKeys("tomsmith");
+        driver.findElement(By.cssSelector("#password")).sendKeys("SuperSecretPassword!");
+        driver.findElement(By.cssSelector(".fa-sign-in")).click();
+
+        Assert.assertTrue(driver.findElement(By.cssSelector(".success")).isDisplayed());
+    }
+
+    @AfterTest
+    public void tearDown(){
+        driver.quit();
+    }
+}
